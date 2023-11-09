0255e5e2aeb2fa7df056c2b28feedc6f1faf2c83
constructorlib <constructorlib@gmail.com>
2023-11-09 11:18:38 +0900

    9 mui icons update for sidebar

--- cherrysocial/src/components/Sidebar/Sidebar.jsx
+++ cherrysocial/src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,53 @@
 import "./sidebar.css";
 import React from "react";
+import * as Icons from "@mui/icons-material";
+import { Icon } from "@mui/material";
 
 export default function Sidebar() {
   return (
     <>
-      <div className="sidebar">Sidebar</div>
+      <div className="sidebar">
+        <div className="sidebarWrapper">
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <Icons.RssFeed className="sidebarIcon" />
+              <span className="sidebarListItemText">Feed</span>
+            </li>
+            <li className="sidebarListItem">
+              <Icons.Chat className="sidebarIcon" />
+              <span className="sidebarListItemText">Chats</span>
+            </li>
+            <li className="sidebarListItem">
+              <Icons.PlayCircle className="sidebarIcon" />
+              <span className="sidebarListItemText">Videos</span>
+            </li>
+            <li className="sidebarListItem">
+              <Icons.Groups className="sidebarIcon" />
+              <span className="sidebarListItemText">Groups</span>
+            </li>
+            <li className="sidebarListItem">
+              <Icons.Bookmark className="sidebarIcon" />
+              <span className="sidebarListItemText">Bookmarks</span>
+            </li>
+            <li className="sidebarListItem">
+              <Icons.Help className="sidebarIcon" />
+              <span className="sidebarListItemText">Questions</span>
+            </li>
+            <li className="sidebarListItem">
+              <Icons.WorkOutline className="sidebarIcon" />
+              <span className="sidebarListItemText">Jobs</span>
+            </li>
+            <li className="sidebarListItem">
+              <Icons.Event className="sidebarIcon" />
+              <span className="sidebarListItemText">Events</span>
+            </li>
+            <li className="sidebarListItem">
+              <Icons.School className="sidebarIcon" />
+              <span className="sidebarListItemText">Questions</span>
+            </li>
+          </ul>
+        </div>
+      </div>
     </>
   );
 }
