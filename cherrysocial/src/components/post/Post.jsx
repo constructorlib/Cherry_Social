28f56708dbea82c0332c3d1dbaece8dcf8b219c8
constructorlib <constructorlib@gmail.com>
2023-11-09 19:11:17 +0900

    done & done

--- cherrysocial/src/components/post/Post.jsx
+++ cherrysocial/src/components/post/Post.jsx
@@ -9,14 +9,26 @@ export default function Post() {
           <div className="postTopLeft">
             <img src="assets/person/1.jpeg" className="postProfileImg" alt="" />
             <span className="postUserName">Dinora Khoshimova</span>
-            <span className="postDate"> 5 mins ago</span>
+            <span className="postDate"> 5 minutes ago</span>
           </div>
           <div className="postTopRight">
             <Icons.MoreVert className="postIcon" />
           </div>
         </div>
-        <div className="postCenter"></div>
-        <div className="postBottom"></div>
+        <div className="postCenter">
+          <span className="postText">Hey, it's my first post =) </span>
+          <img src="assets/post/1.jpeg" alt="" className="postImg" />
+        </div>
+        <div className="postBottom">
+          <div className="postBottomLeft">
+            <img className="likeIcon" src="assets/like.png" alt="" />
+            <img className="likeIcon" src="assets/heart.png" alt="" />
+            <span className="likeCounter">32 people liked this post</span>
+          </div>
+          <div className="postBottomRight">
+            <span className="postCommentText">9 comments</span>
+          </div>
+        </div>
       </div>
     </div>
   );
