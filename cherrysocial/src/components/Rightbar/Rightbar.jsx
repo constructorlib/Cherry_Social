f4c77ce51629a1ca0ddb3fd023734429ebf85045
constructorlib <constructorlib@gmail.com>
2023-11-09 19:38:33 +0900

    finished layout

--- cherrysocial/src/components/Rightbar/Rightbar.jsx
+++ cherrysocial/src/components/Rightbar/Rightbar.jsx
@@ -4,7 +4,86 @@ import React from "react";
 export default function Rightbar() {
   return (
     <>
-      <div className="rightbar">Rightbar</div>
+      <div className="rightbar">
+        <div className="rightbarWrapper">
+          <div className="birthdayContainer">
+            <img src="assets/gift.png" className="birthdayImg" alt="" />
+            <span className="birthdayText">
+              <b>Maria Haim</b> and <b>3 other friends</b> have a birthday today
+            </span>
+          </div>
+          <img src="assets/ad.png" alt="" className="rightbarAd" />
+          <h4 className="rightbarTitle">Online Friends</h4>
+          <ul className="rightbarFriendList">
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img
+                  src="assets/person/4.jpeg"
+                  alt=""
+                  className="rightbarProfileImg"
+                />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">Maria Haim</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img
+                  src="assets/person/4.jpeg"
+                  alt=""
+                  className="rightbarProfileImg"
+                />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">Maria Haim</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img
+                  src="assets/person/4.jpeg"
+                  alt=""
+                  className="rightbarProfileImg"
+                />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">Maria Haim</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img
+                  src="assets/person/4.jpeg"
+                  alt=""
+                  className="rightbarProfileImg"
+                />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">Maria Haim</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img
+                  src="assets/person/4.jpeg"
+                  alt=""
+                  className="rightbarProfileImg"
+                />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">Maria Haim</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img
+                  src="assets/person/4.jpeg"
+                  alt=""
+                  className="rightbarProfileImg"
+                />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">Maria Haim</span>
+            </li>
+          </ul>
+        </div>
+      </div>
     </>
   );
 }
