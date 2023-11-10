73d2aba2b309736a63c44c4d1ce8ad65cbe2abcd
constructorlib <constructorlib@gmail.com>
2023-11-10 22:53:42 +0900

    login page.jsx

--- cherrysocial/src/main.jsx
+++ cherrysocial/src/main.jsx
@@ -4,10 +4,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import Home from "./pages/home/Home.jsx";
 import Profile from "./pages/profile/Profile.jsx";
+import Login from "./pages/login/Login.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <Home /> */}
-    <Profile />
-    {/* <App /> */}
+    {/* <Profile /> */}
+    <Login />
   </React.StrictMode>
 );
