cee46609ad9b58b3811e6a67424f8f85605b9ef0
constructorlib <constructorlib@gmail.com>
2023-11-10 09:06:55 +0900

    aligned postUserName and postDate, included the arrays

--- cherrysocial/src/components/post/Post.jsx
+++ cherrysocial/src/components/post/Post.jsx
@@ -1,7 +1,7 @@
 import "./post.css";
 import * as Icons from "@mui/icons-material";
 
-export default function Post() {
+export default function Post({ post }) {
   return (
     <div className="post">
       <div className="postWrapper">
@@ -9,24 +9,26 @@ export default function Post() {
           <div className="postTopLeft">
             <img src="assets/person/1.jpeg" className="postProfileImg" alt="" />
             <span className="postUserName">Dinora Khoshimova</span>
-            <span className="postDate"> 5 minutes ago</span>
+            <span className="postDate"> {post.date}</span>
           </div>
           <div className="postTopRight">
             <Icons.MoreVert className="postIcon" />
           </div>
         </div>
         <div className="postCenter">
-          <span className="postText">Hey, it's my first post =) </span>
-          <img src="assets/post/1.jpeg" alt="" className="postImg" />
+          <span className="postText">{post?.desc} </span>
+          <img src={post.photo} alt="" className="postImg" />
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
             <img className="likeIcon" src="assets/like.png" alt="" />
             <img className="likeIcon" src="assets/heart.png" alt="" />
-            <span className="likeCounter">32 people liked this post</span>
+            <span className="likeCounter">
+              {post.like} people liked this post
+            </span>
           </div>
           <div className="postBottomRight">
-            <span className="postCommentText">9 comments</span>
+            <span className="postCommentText">{post.comment} comments</span>
           </div>
         </div>
       </div>
