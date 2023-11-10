c260e9df1289efd7e4791891365850a060facb68
constructorlib <constructorlib@gmail.com>
2023-11-10 17:20:26 +0900

    like handler functionality

--- cherrysocial/src/components/post/Post.jsx
+++ cherrysocial/src/components/post/Post.jsx
@@ -1,8 +1,16 @@
 import "./post.css";
 import * as Icons from "@mui/icons-material";
 import { Users } from "../../dummyData";
-
+import React from "react";
+import { useState } from "react";
 export default function Post({ post }) {
+  const [like, setLike] = useState(post.like);
+  const [isLiked, setIsLiked] = useState(false);
+  const likeHandler = () => {
+    setLike(isLiked ? like - 1 : like + 1);
+    setIsLiked(!isLiked);
+  };
+
   return (
     <div className="post">
       <div className="postWrapper">
@@ -28,11 +36,19 @@ export default function Post({ post }) {
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
-            <img className="likeIcon" src="assets/like.png" alt="" />
-            <img className="likeIcon" src="assets/heart.png" alt="" />
-            <span className="likeCounter">
-              {post.like} people liked this post
-            </span>
+            <img
+              className="likeIcon"
+              src="assets/like.png"
+              onClick={likeHandler}
+              alt=""
+            />
+            <img
+              className="likeIcon"
+              src="assets/heart.png"
+              onClick={likeHandler}
+              alt=""
+            />
+            <span className="likeCounter">{like} people liked this post</span>
           </div>
           <div className="postBottomRight">
             <span className="postCommentText">{post.comment} comments</span>
