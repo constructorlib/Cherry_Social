d68f60f5acab076e9a28c059f5ef2d8410407b7d
constructorlib <constructorlib@gmail.com>
2023-11-11 21:25:21 +0900

    mongoose Schema

--- cherrysocial/api/index.js
+++ cherrysocial/api/index.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 dotenv.config();
 
 const app = express();
@@ -28,6 +29,8 @@ app.use(helmet());
 app.use(morgan("common"));
 
 app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
 app.listen(8800, () => {
   console.log("Server running on port 8800");
 });
