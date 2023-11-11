62ef081912805bffcd9b3c6c0cdedebf68f15d79
constructorlib <constructorlib@gmail.com>
2023-11-11 17:11:41 +0900

    oh wow, deprecated mongo, whou would have thought

--- cherrysocial/api/index.js
+++ cherrysocial/api/index.js
@@ -1,3 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+dotenv.config();
+
 const app = express();
-app.listen(8800, () => console.log("server running"));
+
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Mongo connected");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+  }
+}
+
+connectToDatabase();
+
+app.listen(8800, () => {
+  console.log("Server running on port 8800");
+});
