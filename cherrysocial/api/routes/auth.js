31bb06ec8757ee5225dc84b3b1ec4041655f7b65
constructorlib <constructorlib@gmail.com>
2023-11-13 19:28:07 +0900

    postman is elevating my laptop to sapiens

--- cherrysocial/api/routes/auth.js
+++ cherrysocial/api/routes/auth.js
@@ -3,14 +3,17 @@ const User = require("../models/User");
 
 // REGISTER
 router.get("/register", async (req, res) => {
-  const newUser = await new User({
-    username: "john",
-    email: "asdsa@gmail.com",
-    password: "123456",
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
   });
-
-  await newUser.save(); // <-- Fix the typo here
-  res.send("ok");
+  try {
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
