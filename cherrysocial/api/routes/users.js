3147f227375c308a1a9c7a38ce1b8e575a562058
constructorlib <constructorlib@gmail.com>
2023-11-14 15:30:48 +0900

    password change if a matches user id through post

--- cherrysocial/api/routes/users.js
+++ cherrysocial/api/routes/users.js
@@ -1,6 +1,24 @@
 const router = require("express").Router();
-
-router.get("/", (req, res) => {
-  res.send("user route");
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+//update user
+router.put("/:id", async (req, res) => {
+  if (req.body.userID === req.params.id || req.user.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (err) {
+        return res.status.json(err);
+      }
+    }
+  } else {
+    return res.status(403).json("You cannot update this info");
+  }
 });
+//delete user
+//getsin user
+//follow user
+//follow not
+
 module.exports = router;
