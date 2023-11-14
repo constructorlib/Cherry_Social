e9ac0e223bced8fd8f86dcd36899a35e8386c8a4
constructorlib <constructorlib@gmail.com>
2023-11-14 21:13:16 +0900

    finish later, I have 3 assignments due

--- cherrysocial/api/routes/users.js
+++ cherrysocial/api/routes/users.js
@@ -9,8 +9,16 @@ router.put("/:id", async (req, res) => {
         const salt = await bcrypt.genSalt(10);
         req.body.password = await bcrypt.hash(req.body.password, salt);
       } catch (err) {
-        return res.status.json(err);
+        return res.status(500).json(err);
       }
+    }
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("Account has been updated");
+    } catch {
+      return res.status(500).json(err);
     }
   } else {
     return res.status(403).json("You cannot update this info");
