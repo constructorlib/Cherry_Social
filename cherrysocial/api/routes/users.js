a8269452c1eea36879e9353109cf59b8579f74c4
constructorlib <constructorlib@gmail.com>
2023-11-15 22:04:06 +0900

    catch was throwing error (is it still?)

--- cherrysocial/api/routes/users.js
+++ cherrysocial/api/routes/users.js
@@ -3,7 +3,7 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 //update user
 router.put("/:id", async (req, res) => {
-  if (req.body.userID === req.params.id || req.user.isAdmin) {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
     if (req.body.password) {
       try {
         const salt = await bcrypt.genSalt(10);
@@ -17,13 +17,14 @@ router.put("/:id", async (req, res) => {
         $set: req.body,
       });
       res.status(200).json("Account has been updated");
-    } catch {
+    } catch (err) {
       return res.status(500).json(err);
     }
   } else {
-    return res.status(403).json("You cannot update this info");
+    return res.status(403).json("You can update only your account!");
   }
 });
+
 //delete user
 //getsin user
 //follow user
