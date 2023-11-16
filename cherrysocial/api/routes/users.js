d5ffde0b93a99557714af895ca7745bd3ec08718
constructorlib <constructorlib@gmail.com>
2023-11-16 20:45:47 +0900

    followerS to FOLLOWER

--- cherrysocial/api/routes/users.js
+++ cherrysocial/api/routes/users.js
@@ -57,8 +57,11 @@ router.put("/:id/follow", async (req, res) => {
   if (req.body.userId !== req.params.id) {
     try {
       const user = await User.findById(req.params.id);
+      console.log(user);
       const currentUser = await User.findById(req.body.userId);
-      if (!user.followers.includes(req.body.userId)) {
+      console.log(currentUser);
+
+      if (!user?.follower.includes(req.body.userId)) {
         await user.updateOne({ $push: { followers: req.body.userId } });
         await currentUser.updateOne({
           $push: { followings: req.params.id },
