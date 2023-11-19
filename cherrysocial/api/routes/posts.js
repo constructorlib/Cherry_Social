47171cd33b4a111afcd92dc74382272dfd6e644f
constructorlib <constructorlib@gmail.com>
2023-11-19 21:52:57 +0900

    delete post model

--- cherrysocial/api/routes/posts.js
+++ cherrysocial/api/routes/posts.js
@@ -29,6 +29,20 @@ router.put("/:id", async (req, res) => {
   }
 });
 //delete a post
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      res.status(200).json("the post has been deleted");
+    } else {
+      res.status(403).json("you can delete only your post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 //get a post
 //call timeline post
 // works, do it tomorrow. Tired today
