d2f1a30d48248cf8b4f0b2bf32d77861326dcbc5
constructorlib <constructorlib@gmail.com>
2023-11-20 16:40:14 +0900

    dotenv stupid

--- cherrysocial/client/src/main.jsx
+++ cherrysocial/client/src/main.jsx
@@ -1,3 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config();
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
