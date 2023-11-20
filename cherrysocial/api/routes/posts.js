d729ff9a583869aa25b4e0d4acfeb461a8f0db17
constructorlib <constructorlib@gmail.com>
2023-11-20 09:10:03 +0900

    with no res.status, postman goes into error loop

--- cherrysocial/api/routes/posts.js
+++ cherrysocial/api/routes/posts.js
@@ -49,9 +49,10 @@ router.put("/:id/like", async (req, res) => {
     const post = await Post.findById(req.params.id);
     if (!post.likes.includes(req.body.userId)) {
       await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json("You liked this post!");
     } else {
       await post.updateOne({ $pull: { likes: req.body.userId } });
-      res.status(200).json("Like has been removed ");
+      res.status(200).json("Like has been removed!");
     }
   } catch (err) {
     res.status(500).json(err);
