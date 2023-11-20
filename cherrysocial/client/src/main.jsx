2faa04fa41597ffecebf8c51668833858c5b8bb5
constructorlib <constructorlib@gmail.com>
2023-11-20 15:48:03 +0900

    update

--- cherrysocial/client/src/main.jsx
+++ cherrysocial/client/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home.jsx";
 import Profile from "./pages/profile/Profile.jsx";
 import Login from "./pages/login/Login.jsx";
+import Register from "./pages/register/Register.jsx";
 import "./index.css";
 
 const root = document.getElementById("root");
@@ -11,9 +12,10 @@ const root = document.getElementById("root");
 ReactDOM.createRoot(root).render(
   <Router>
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route exact path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/profile/:username" element={<Profile />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   </Router>
 );
