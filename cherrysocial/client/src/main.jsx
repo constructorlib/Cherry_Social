cbaa395d76db0c27aece2c2aec3bd55f3fc1a92d
constructorlib <constructorlib@gmail.com>
2023-11-20 14:41:36 +0900

    css conflict

--- cherrysocial/client/src/main.jsx
+++ cherrysocial/client/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home.jsx";
 import Profile from "./pages/profile/Profile.jsx";
 import Login from "./pages/login/Login.jsx";
+import "./index.css";
 
 const root = document.getElementById("root");
 
