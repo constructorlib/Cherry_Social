25915049f253b327b7c203eba18102e3cdf2e2a9
constructorlib <constructorlib@gmail.com>
2023-11-20 14:23:54 +0900

    move to router dom (css died, check it)

--- cherrysocial/client/src/main.jsx
+++ cherrysocial/client/src/main.jsx
@@ -1,27 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-// import App from "./App.jsx";
-import "./index.css";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home.jsx";
 import Profile from "./pages/profile/Profile.jsx";
 import Login from "./pages/login/Login.jsx";
-import { createRoot } from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Routes,
-  Router,
-} from "react-router-dom";
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/profile/:username" element={<Profile />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/profile/:username" element={<Profile />} />
+    </Routes>
+  </Router>
+);
