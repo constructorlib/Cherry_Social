df524a69a866ed90be83656c4d7ed3be51574675
constructorlib <constructorlib@gmail.com>
2023-11-20 09:08:08 +0900

    like/dislike

--- cherrysocial/api/routes/posts.js
+++ cherrysocial/api/routes/posts.js
@@ -43,7 +43,20 @@ router.delete("/:id", async (req, res) => {
     res.status(500).json(err);
   }
 });
-//get a post
+//like post
+router.put("/:id/like", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).json("Like has been removed ");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 //call timeline post
 // works, do it tomorrow. Tired today
 module.exports = router;
