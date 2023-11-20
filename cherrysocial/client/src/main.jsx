c84a2d5e82b16d9c3a763627c1f0a700db642940
constructorlib <constructorlib@gmail.com>
2023-11-20 14:01:23 +0900

    router (which is unexpectedly not working)

--- cherrysocial/client/src/main.jsx
+++ cherrysocial/client/src/main.jsx
@@ -5,10 +5,32 @@ import "./index.css";
 import Home from "./pages/home/Home.jsx";
 import Profile from "./pages/profile/Profile.jsx";
 import Login from "./pages/login/Login.jsx";
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    {/* <Home /> */}
-    {/* <Profile /> */}
-    <Login />
-  </React.StrictMode>
-);
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Router,
+} from "react-router-dom";
+
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+      <Switch>
+        <Route path="/profile/:username">
+          <Profile />
+        </Route>
+      </Switch>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
