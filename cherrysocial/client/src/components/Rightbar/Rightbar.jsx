9040ac456cbc53901e9b8c61e29e8c5f5d1838e1
constructorlib <constructorlib@gmail.com>
2023-11-22 14:22:24 +0900

    some UI changes

--- cherrysocial/client/src/components/Rightbar/Rightbar.jsx
+++ cherrysocial/client/src/components/Rightbar/Rightbar.jsx
@@ -5,21 +5,31 @@ import Online from "../online/Online";
 import { Key } from "@mui/icons-material";
 export default function Rightbar() {
   const PF = import.meta.env.VITE_REACT_APP_PUBLIC_FOLDER;
+  const HomeRightBar = () => {
+    return (
+      <>
+        <div className="birthdayContainer">
+          <img src="assets/gift.png" className="birthdayImg" alt="" />
+          <span className="birthdayText">
+            <b>Maria Haim</b> and <b>3 other friends</b> have a birthday today
+          </span>
+        </div>
+        <img src="assets/ad.jpg" alt="" className="rightbarAd" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          {PF + Users.map((u) => <Online key={u.id} user={u} />)}
+        </ul>
+      </>
+    );
+  };
+  const ProfileRightBar = () => {
+    return <h1>Wrapper</h1>;
+  };
   return (
     <>
       <div className="rightbar">
         <div className="rightbarWrapper">
-          <div className="birthdayContainer">
-            <img src="assets/gift.png" className="birthdayImg" alt="" />
-            <span className="birthdayText">
-              <b>Maria Haim</b> and <b>3 other friends</b> have a birthday today
-            </span>
-          </div>
-          <img src="assets/ad.jpg" alt="" className="rightbarAd" />
-          <h4 className="rightbarTitle">Online Friends</h4>
-          <ul className="rightbarFriendList">
-            {PF + Users.map((u) => <Online key={u.id} user={u} />)}
-          </ul>
+          <ProfileRightBar />
         </div>
       </div>
     </>
