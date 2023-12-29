6c5469bd347571669171a7a51fd5f972e77dbd79
constructorlib <constructorlib@gmail.com>
2023-12-29 23:13:27 +0900

    prevent refresh

--- cherrysocial/client/src/components/Feed/Feed.jsx
+++ cherrysocial/client/src/components/Feed/Feed.jsx
@@ -1,30 +1,21 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "../post/Post";
 import Share from "../share/Share";
 import "./feed.css";
-// import { Posts } from "../../dummyData";
 import axios from "axios";
-export default function Feed() {
-  const [posts, setPosts] = useState([]);
 
+export default function Feed({ username }) {
+  const [posts, setPosts] = useState([]);
   useEffect(() => {
-    const proxy = import.meta.env.VITE_PROXY;
-    const fetchPosts = async () => {
-      const res = await axios.get(
-        `${proxy}/posts/timeline/6551d3f81720514eff91624a`
-      );
-      console.log("FOOOOOOOOOOOOO");
-      console.log(res);
-    };
-    fetchPosts();
+    console.log("feed rendered");
   }, []);
   return (
     <div className="feed">
       <div className="feedWrapper">
-        <Share />
-        {/* {Posts.map((p) => (
-          <Post key={p.id} post={p} />
-        ))} */}
+        {(!username || username === user.username) && <Share />}
+        {posts.map((p) => (
+          <Post key={p._id} post={p} />
+        ))}
       </div>
     </div>
   );
